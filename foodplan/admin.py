0f8621f8522a02c1e438ec6ae5a0c7fee44eda50
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2024-04-17 12:15:18 +0300

    Add models

--- foodplan/admin.py
+++ foodplan/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from foodplan.models import MealType, Allergy, Ingredient, MenuType, Recipe, RecipeIngredient
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = (RecipeIngredientInline,)
+
+
+@admin.register(MenuType)
+class MenuTypeAdmin(admin.ModelAdmin):
+    list_display = [ 'name', 'preview', ]
+    readonly_fields = ['preview', ]
+
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 150px;">')
+
+
+admin.site.register(MealType)
+admin.site.register(Allergy)
+admin.site.register(Ingredient)
+admin.site.register(RecipeIngredient)
--- foodplan/models.py
+++ foodplan/models.py
@@ -1,3 +1,107 @@
 from django.db import models
 
-# Create your models here.
+
+class MealType(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Тип блюда')
+
+    class Meta:
+        verbose_name = 'Тип блюда'
+        verbose_name_plural = 'Типы блюд'
+
+    def __str__(self):
+        return self.name
+
+
+class Allergy(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Тип аллергии')
+
+    class Meta:
+        verbose_name = 'Тип аллергии'
+        verbose_name_plural = 'Типы аллергии'
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Ингредиент')
+    allergy = models.ForeignKey(Allergy,
+                                on_delete=models.SET_NULL,
+                                verbose_name='Аллергия',
+                                related_name='ingredients',
+                                null=True,
+                                blank=True,
+                                )
+    caloricity = models.IntegerField(verbose_name='Калорийность, ккал')
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name
+
+
+class MenuType(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Диета')
+    image = models.ImageField(verbose_name='Изображение', upload_to='diets')
+
+    class Meta:
+        verbose_name = 'Тип меню'
+        verbose_name_plural = 'Типы меню'
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Название рецепта')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(verbose_name='Изображение', upload_to='recipes')
+    ingredients = models.ManyToManyField(Ingredient,
+                                         verbose_name='Ингредиенты',
+                                         related_name='recipes',
+                                         through='RecipeIngredient',
+                                         )
+    menu_type = models.ForeignKey(MenuType,
+                                  on_delete=models.PROTECT,
+                                  verbose_name='Тип меню',
+                                  related_name='recipes',
+                                  )
+    meal_type = models.ForeignKey(MealType,
+                                  on_delete=models.PROTECT,
+                                  verbose_name='Тип блюда',
+                                  related_name='recipes',
+                                  )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+    def total_calories(self):
+        return sum([ingredient.quantity * (ingredient.ingredient.caloricity / 100)
+                    for ingredient in self.ingredients.all()])
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe,
+                               on_delete=models.PROTECT,
+                               verbose_name='Рецепт',
+                               related_name='recipe_ingredients',
+                               )
+    ingredient = models.ForeignKey(Ingredient,
+                                   on_delete=models.PROTECT,
+                                   verbose_name='Ингредиент',
+                                   related_name='recipe_ingredients',
+                                   )
+    quantity = models.IntegerField(verbose_name='Количество, г')
+
+    class Meta:
+        verbose_name = 'Ингредиент рецепта'
+        verbose_name_plural = 'Ингредиенты рецепта'
+
+    def __str__(self):
+        return f'{self.ingredient} - {self.quantity}'
